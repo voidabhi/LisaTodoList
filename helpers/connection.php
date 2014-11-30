80a9313ec420c523bbd16adb3ea2226e833df8c8
Abhijeet <void.aby@gmail.com>
2014-11-30 21:22:14 +0530

    updated database name

--- helpers/connection.php
+++ helpers/connection.php
@@ -4,7 +4,7 @@
         try{
             $username = 'adminc9AFVDJ';
             $password = 'L87Mum1A__qf';
-            $conn = new pdo("mysql:host=127.7.34.130;dbname=lisadb;", $username, $password);
+            $conn = new pdo("mysql:host=127.7.34.130;dbname=lisatodolist;", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
 
