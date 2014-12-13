531745588ff0a90c65602c647b0f3c0ac7b11e45
Abhijeet Mohan <void.aby@gmail.com>
2014-12-13 23:43:27 +0530

    fixed typo

--- helpers/connection.php
+++ helpers/connection.php
@@ -5,7 +5,7 @@
             $mysql = "mysql:host=127.7.34.130;dbname=lisatodolist";
             $username = 'adminc9AFVDJ';
             $password = 'L87Mum1A__qf';
-            $conn = new pdo("$mysql", $username, $password);
+            $conn = new pdo($mysql, $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
 
