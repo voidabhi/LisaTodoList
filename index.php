2fb827af327d9439ad2021ffe9e0eca67aadfc5f
Abhijeet Mohan <void.aby@gmail.com>
2015-06-04 17:38:07 +0530

    removing commented

--- index.php
+++ index.php
@@ -18,13 +18,7 @@
     <!-- Bootstrap -->
     <link href="static/css/bootstrap.min.css" rel="stylesheet">
     <link href="static/css/style.css" rel="stylesheet">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    
   </head>
   <body>
     
@@ -60,7 +54,6 @@
       </form>
 
     </div> 
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
     <script src="static/js/bootstrap.min.js"></script>
   </body>
 </html>
